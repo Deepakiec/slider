530da88d85e1e7e044f99a26a0f9d18f7fb64ad8
Deepak120 <Deepakiec2009@gmail.com>
2014-05-17 23:48:47 -0700

    Create TaskCollection.js

---
+++ js/TaskCollection.js
@@ -0,0 +1,9 @@
+var TaskCollection = Backbone.Collection.extend({
+
+	model:TaskModel,
+	 initialize: function(){
+            console.log("TaskCollection.js>>>initialize");
+        }
+	
+     
+    });
